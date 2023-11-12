f7fdb47ca6177f13bf8431e301b4fd8cf988fc36
Xanderelsmith@gmail.com <xanderelsmith@gmail.com>
2023-11-12 21:22:43 +0100

    object techniques

---
+++ objecttechniques.py
@@ -0,0 +1,13 @@
+class Laptop:
+    def __init__(self,name,sn):
+        sn=0
+        Laptop.sn
+        self.name=name
+        
+dell=Laptop('dellg3')
+hp=Laptop('Hp1101')
+dell.price=100       
+print(dell.name)
+print(dell.price)
+print(hp.name)
+print(hp.price)
