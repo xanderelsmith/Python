c65a7ef661d7780026e48e61466ec0f78865123f
xander <xanderelsmith@gmail.com>
2024-06-28 08:53:49 +0100

    challenge

---
+++ list/nestedlists.py
@@ -0,0 +1,5 @@
+fruits=['apple','orange','pineapple','pear']
+vegetables=['spinach','carrot','tomato']
+
+plants=[fruits,vegetables]
+print(plants)
---
+++ list/treasuremap.py
@@ -0,0 +1,8 @@
+line1=['🔳','🔳','🔳']
+line2=['🔳','🔳','🔳']
+line3=['🔳','🔳','🔳']
+map=[line1,line2,line3]
+print('Hiding your treasure! x marks the spot. ')
+position = int(input('Where do you want to put the treasure?'))
+# ❌
+print(f"{line1}\n{line2}\n{line3}")
