488f7d98ee18eaf092c0992e2c56d52e757c2205
xander <xanderelsmith@gmail.com>
2024-09-15 20:19:13 +0100

    stage3 day 7

--- day7/stage2.py
+++ day7/stage2.py
@@ -4,11 +4,12 @@ word_list=["aadvark","crocodile","fish"]
 
 chosenword = random.choice(word_list)
 print(chosenword)
+# TODO-1 - Create a "placeholder" with the same number of blanks as 
+
 placholder=''
 for textindex in range(0,len(chosenword)):
    placholder+='_'
 print(placholder)
-# TODO-1 - Create a "placeholder" with the same number of blanks as 
 
 guess=input('Guess a letter:\t').lower()
 # TODO-2 - Create a "display" that puts the guess letter in the right 
---
+++ day7/stage3.py
@@ -0,0 +1,41 @@
+import random
+word_list=["aadvark","crocodile","fish"]
+
+answerwordlist=[]
+chosenword = random.choice(word_list).lower()
+# print(chosenword)
+# TODO-1 - Use a while loop to let the User guess again 
+
+fields=''
+for textindex in range(0,len(chosenword)):
+   fields+='_'
+print(fields)
+
+answerstate=''
+placeholder=''
+while (placeholder!=chosenword):
+    placeholder=''
+    guess=input('Guess a letter:\t').lower()
+    # TODO-2 - Change the for loop so that you keep the previous correct     
+    for textindex in chosenword:
+        text = textindex             
+        if guess==text:
+            if (not answerwordlist.__contains__(guess)):
+                answerwordlist.append(guess)
+            
+            placeholder+=guess
+        elif(answerwordlist.__contains__(text)):
+            placeholder+=text           
+        else:
+            placeholder+='_'
+    print(placeholder)
+    
+    # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it is 
+    # "wrong" if it's true
+    if (chosenword.__contains__(guess)):
+        print('true')
+    else:
+        print('False')
+    
+        
+
