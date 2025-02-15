fcbc5f23802a88710faf12ab176b5f82f3c72686
unknown <xanderelsmith@gmail.com>
2025-02-15 14:59:00 +0100

    revamp screen

--- day10/secretaunctionprogram.py
+++ day10/secretaunctionprogram.py
@@ -1,5 +1,33 @@
-print('Welcome to the scret a1unction program')
-name =input('What is your name?:\t')
-bid = input('What\'s your bid?: \t$')
-response= input("Are there any other bidders? Type 'yes' or 'no' ")
-response= input('')
+moreUser="yes"
+biddingDocuments={  }
+
+
+def getHighestBidder(biddingDocuments):
+    for bids in biddingDocuments:
+        winner=''
+        currentBid=0
+        if currentBid > biddingDocuments[bids]:
+            currentBid=biddingDocuments[bids]
+            print(winner)
+            winner=bids
+        return winner
+            
+    
+    
+    
+while moreUser=="yes":
+    print('Welcome to the scret aunction program')
+    name =input('What is your name?:\t')
+    bid = input('What\'s your bid?: \t$')
+    biddingDocuments['name'] = int(bid)
+    response= input("Are there any other bidders? Type 'yes' or 'no' ")
+    moreUser=response.lower()
+    if moreUser=="no":
+        winner=getHighestBidder(biddingDocuments)
+        print(f'The winner is {winner} with a bid of ${biddingDocuments[winner]}')
+    else:
+        print('\n'*100)
+
+
+
+    
