9aca02441c39b75afe42b43b01e6d8d927af287c
unknown <xanderelsmith@gmail.com>
2025-02-16 06:26:26 +0100

    day10 complete

--- day10/secretaunctionprogram.py
+++ day10/secretaunctionprogram.py
@@ -1,14 +1,15 @@
 moreUser="yes"
-biddingDocuments={  }
+biddingDocuments={}
 
 
 def getHighestBidder(biddingDocuments):
     for bids in biddingDocuments:
+        print(bids)
         winner=''
         currentBid=0
-        if currentBid > biddingDocuments[bids]:
+        if currentBid < biddingDocuments[bids]:
             currentBid=biddingDocuments[bids]
-            print(winner)
+            print(winner,"winner")
             winner=bids
         return winner
             
@@ -19,12 +20,12 @@ while moreUser=="yes":
     print('Welcome to the scret aunction program')
     name =input('What is your name?:\t')
     bid = input('What\'s your bid?: \t$')
-    biddingDocuments['name'] = int(bid)
+    biddingDocuments[name] = int(bid)
     response= input("Are there any other bidders? Type 'yes' or 'no' ")
     moreUser=response.lower()
     if moreUser=="no":
-        winner=getHighestBidder(biddingDocuments)
-        print(f'The winner is {winner} with a bid of ${biddingDocuments[winner]}')
+        winner = getHighestBidder(biddingDocuments)
+        print(f"The winner is {winner} with a bid of $ {biddingDocuments[winner]}")
     else:
         print('\n'*100)
 
